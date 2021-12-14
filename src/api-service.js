f76b5c62601291eba98fd745953db58986d107e0
OlgaKhlivnenko <xlivnenko@ukr.net>
2021-12-14 09:18:38 -0500

    14-12

--- src/api-service.js
+++ src/api-service.js
@@ -1,21 +1,25 @@
+import axios from "axios";
+import Notiflix from 'notiflix';
+
 export default class APIService {
     constructor() {
         this.searchQuery = '';
         this.page = 1;
     }
 
-    fetchHits(){
-        return fetch(`https://pixabay.com/api/?key=24781056-75ab6f95c382245b51e5e78bf&q=${this.searchQuery}&per_page=3&page=${this.page}&orientation=horizontal&safesearch=true&image_type=photo`)
-            .then(response => response.json())
-            .then(data => {
-                this.page += 1;
-                console.log(data);
-                return data.hits;
-            })
-            .catch(error => {
-                Notiflix.Notify.info('Sorry, there are no images matching your search query. Please try again.');
+    async fetchHits(){
+    try { const response = await axios.get(`https://pixabay.com/api/?key=24781056-75ab6f95c382245b51e5e78bf&q=${this.searchQuery}&per_page=3&page=${this.page}&orientation=horizontal&safesearch=true&image_type=photo`)
+            this.page += 1;
+    console.log(response);
+        console.log(response.data.hits);
+        if (response.data.hits.length === 0) {
+            return Notiflix.Notify.info('Sorry, there are no images matching your search query. Please try again.');
+        }
+            return response.data.hits;
+        }
+            catch (error) {   
                 console.log(error);
-            });
+            }
     }
     resetPage() {
         this.page = 1;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 // import '../css/styles.css';
-import Notiflix from 'notiflix';
-import axios from "axios";
+
+
 import APIService from './api-service';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Your API key: 24781056-75ab6f95c382245b51e5e78bf
 // Notiflix.Notify.info('Sorry, there are no images matching your search query. Please try again.');
 
 const formEl = document.querySelector('.search-form');
-const btnSearch = document.querySelector('[type="submit"]');
 const btnLoadMore = document.querySelector('.load-more');
 const galleryConteiner = document.querySelector('.gallery');
 const apiService = new APIService;
@@ -27,15 +29,10 @@ function onSearch(evt) {
   apiService.resetPage();
   apiService.fetchHits()
     .then(hits => {
-    galleryConteiner.innerHTML = '';
-    markupCards(hits);
-    btnLoadMore.removeAttribute(`disabled`, true);
-    })
-    // .catch(error => {
-    //             Notiflix.Notify.info('Sorry, there are no images matching your search query. Please try again.');
-    //             console.log(error);
-    //         });
-    
+      galleryConteiner.innerHTML = '';
+      markupCards(hits);
+      btnLoadMore.removeAttribute(`disabled`, true);
+    }); 
  
 }
 function onLoadMore() {
@@ -46,7 +43,10 @@ function markupCards(hits) {
    
    galleryConteiner.innerHTML = hits
         .map(({ webformatURL, largeImageURL, likes, views, comments, downloads, }) =>
-            `<div class="photo-card">
+        {
+          return `<a class="gallery__item" href="${largeImageURL}">
+            <div class="photo-card">
+            
   <img src="${webformatURL}" alt="" loading="lazy" />
   <div class="info">
     <p class="info-item">
@@ -66,5 +66,6 @@ function markupCards(hits) {
     ${downloads}
     </p>
   </div>
-</div>`).join('');
+</div></a>`;}).join('');
 };
+const lightbox = new SimpleLightbox('.gallery a');
