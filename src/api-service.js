c05f54852215455eb5252329f11c1ae3a17cbf62
OlgaKhlivnenko <xlivnenko@ukr.net>
2021-12-21 15:08:39 +0200

    21-12

--- src/api-service.js
+++ src/api-service.js
@@ -9,16 +9,11 @@ export default class APIService {
 
     async fetchHits(){
     try { const response = await axios.get(`https://pixabay.com/api/?key=24781056-75ab6f95c382245b51e5e78bf&q=${this.searchQuery}&per_page=40&page=${this.page}&orientation=horizontal&safesearch=true&image_type=photo`)
-           const allFreeImg = response.data.totalHits;
-        Notiflix.Notify.success(`Hooray! We found ${allFreeImg} images`);
-        
-            this.page += 1;
-    console.log(response);
-    console.log(response.data.hits);
+       this.page += 1;
         if (response.data.hits.length === 0) {
             return Notiflix.Notify.info('Sorry, there are no images matching your search query. Please try again.');
         }
-            return response.data.hits;
+            return response.data;
         }
             catch (error) {   
                 console.log(error);
--- src/index.js
+++ src/index.js
@@ -3,9 +3,8 @@ import './css/styles.css';
 import APIService from './api-service';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
+import Notiflix from 'notiflix';
 
-// Your API key: 24781056-75ab6f95c382245b51e5e78bf
-// Notiflix.Notify.info('Sorry, there are no images matching your search query. Please try again.');
 
 const formEl = document.querySelector('.search-form');
 const btnLoadMore = document.querySelector('.load-more');
@@ -20,7 +19,7 @@ function onSearch(evt) {
   evt.preventDefault();
   
   apiService.searchQuery = evt.target.elements.searchQuery.value;
-  const valueInput = apiService.searchQuery.trim()
+  const valueInput = apiService.searchQuery.trim();
   if (valueInput === '') {
     return 
   }
@@ -28,27 +27,33 @@ function onSearch(evt) {
 
   apiService.resetPage();
   apiService.fetchHits()
-    .then(hits => {
-      console.log(hits);
+    .then(data => {
+       const allFreeImg = data.totalHits;
+    Notiflix.Notify.success(`Hooray! We found ${allFreeImg} images`);
       galleryConteiner.innerHTML = '';
+      const hits = data.hits;
       markupCards(hits);
       btnLoadMore.removeAttribute(`disabled`, true);
+      
     }); 
  
 }
 function onLoadMore() {
-  apiService.fetchHits().then(markupCards);
+  apiService.fetchHits().then(data => {
+    markupCards(data.hits)
+    lightbox.refresh()
+  });
 }
 
 function markupCards(hits) {
    
   const markup = hits
-        .map(({ webformatURL, largeImageURL, likes, views, comments, downloads, }) =>
+        .map(({ largeImageURL, webformatURL, likes, views, comments, downloads, }) =>
         {
           return `
-            <div class="photo-card">
-   <a class="gallery__item" href="${largeImageURL}">         
-  <img src="${webformatURL}" alt="" loading="lazy" />
+  <a class="gallery__item" href="${largeImageURL}">
+  <div class="photo-card">      
+  <img class="img-item" src="${webformatURL}" alt="#" loading="lazy" />
   <div class="info">
     <p class="info-item">
       <b>Likes</b>
@@ -67,8 +72,8 @@ function markupCards(hits) {
     ${downloads}
     </p>
   </div>
-  </a>
-</div>`;
+</div>
+</a>`;
         }).join('');
   galleryConteiner.insertAdjacentHTML('beforeend', markup);
 };
